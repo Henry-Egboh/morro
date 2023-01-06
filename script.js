c7a9bd7dd832381ec8f048283eb0bde305c0434c
Henry-Eg <107496624+Henry-Egboh@users.noreply.github.com>
2023-01-06 15:28:16 +0100

    A complete web page for educational purposes

---
+++ script.js
@@ -0,0 +1,7 @@
+// alert("DOM Manipulation Exercise!");
+var companyName = document.getElementById("head");
+var showP = document.getElementsByClassName("show_txt");
+var showH = document.getElementsByTagName("h3");
+var list = document.querySelectorAll("li");
+// var main = document.querySelector("main");
+var h3 = document.querySelectorAll("h3");
